f12e3f6bbadcd4f681614799fd05d7e40e7bfad9
dllo <1434100874@qq.com>
2017-02-22 16:39:48 +0800

    主页的四个按钮 -----------赵子文

--- MyEverybodyVideo/app/src/main/java/com/ziyuanziwen/everybodyvideo/myeverybodyvideo/MainActivity.java
+++ MyEverybodyVideo/app/src/main/java/com/ziyuanziwen/everybodyvideo/myeverybodyvideo/MainActivity.java
@@ -1,13 +1,70 @@
 package com.ziyuanziwen.everybodyvideo.myeverybodyvideo;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.ziyuanziwen.everybodyvideo.myeverybodyvideo.base.BaseActivity;
+import com.ziyuanziwen.everybodyvideo.myeverybodyvideo.homepage.HomePageFragment;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+
+    private RadioButton btnHomePage;
+    private RadioButton btnChannel;
+    private RadioButton btnFind;
+    private RadioButton btnMine;
+    private HomePageFragment homePageFragment;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected int getLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+        btnHomePage = byView(R.id.mainAty_rbtHomePage);
+        btnChannel = byView(R.id.mainAty_rbtChannel);
+        btnFind = byView(R.id.mainAty_rbtFind);
+        btnMine = byView(R.id.mainAty_rbtMine);
+
+        btnChannel.setOnClickListener(this);
+        btnFind.setOnClickListener(this);
+        btnHomePage.setOnClickListener(this);
+        btnMine.setOnClickListener(this);
+
+        homePageFragment = new HomePageFragment();
+
+
+    }
+
+
+    @Override
+    protected void initData() {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.mainAty_frameLayout, homePageFragment);
+        transaction.commit();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        switch (view.getId()) {
+            case R.id.mainAty_rbtHomePage:
+                transaction.replace(R.id.mainAty_frameLayout, homePageFragment);
+                break;
+            case R.id.mainAty_rbtChannel:
+                break;
+            case R.id.mainAty_rbtFind:
+                break;
+            case R.id.mainAty_rbtMine:
+                break;
+        }
+        transaction.commit();
+
     }
 }
---
+++ MyEverybodyVideo/app/src/main/java/com/ziyuanziwen/everybodyvideo/myeverybodyvideo/homepage/HomePageFragment.java
@@ -0,0 +1,11 @@
+package com.ziyuanziwen.everybodyvideo.myeverybodyvideo.homepage;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by dllo on 17/2/22.
+ */
+
+public class HomePageFragment extends Fragment {
+
+}
