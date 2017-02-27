19c1550418770f891b650f6dba088497c00a7bd3
dllo <1434100874@qq.com>
2017-02-27 17:26:04 +0800

    精选界面(适配器未完成)

--- MyEverybodyVideo/app/src/main/java/com/ziyuanziwen/everybodyvideo/myeverybodyvideo/homepage/choice/ChoiceFragment.java
+++ MyEverybodyVideo/app/src/main/java/com/ziyuanziwen/everybodyvideo/myeverybodyvideo/homepage/choice/ChoiceFragment.java
@@ -6,8 +6,16 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.ziyuanziwen.everybodyvideo.myeverybodyvideo.R;
 import com.ziyuanziwen.everybodyvideo.myeverybodyvideo.base.BaseFragment;
+import com.ziyuanziwen.everybodyvideo.myeverybodyvideo.net.OnNetResultListener;
+import com.ziyuanziwen.everybodyvideo.myeverybodyvideo.net.VolleyManager;
+import com.ziyuanziwen.everybodyvideo.myeverybodyvideo.util.ToastTool;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by dllo on 17/2/22.
@@ -18,6 +26,17 @@ import com.ziyuanziwen.everybodyvideo.myeverybodyvideo.base.BaseFragment;
 public class ChoiceFragment extends BaseFragment {
 
     private RecyclerView choiceRv;
+    private ChoiceAdapter adapter;
+    //    up主推荐集合
+    private List<ChoiceEntity.DataBean.RecommendUpBean> upBeanList;
+    //    轮播图集合
+    private List<ChoiceEntity.DataBean.BannerTopBean> bannerBeanList;
+    //    奥斯卡集合
+    private List<ChoiceEntity.DataBean.OfficalAlbumBean> resultListBeanList;
+    //    今日推荐集合
+    private List<ChoiceEntity.DataBean.TodayRecommendBean> todayRecommendBeanList;
+    //    分类集合
+    private List<ChoiceEntity.DataBean.CategoryListBean> categoryListBeanList;
 
     //    绑定布局
     @Override
@@ -25,7 +44,7 @@ public class ChoiceFragment extends BaseFragment {
         return R.layout.fragment_choice;
     }
 
-//    使基类中标题栏消失
+    //    使基类中标题栏消失
     @Override
     protected void initTitle(RelativeLayout titleLayout, ImageView backIv, TextView titleTv, ImageView rightIv) {
         titleLayout.setVisibility(View.GONE);
@@ -40,7 +59,47 @@ public class ChoiceFragment extends BaseFragment {
     @Override
     protected void initData() {
 
+        adapter = new ChoiceAdapter(getContext());
+        getPostRequestContent();
+        choiceRv.setAdapter(adapter);
+
+    }
 
 
+    private void getPostRequestContent() {
+
+        String postUrl = "http://api.rr.tv/v3plus/index/collection";
+        String key = "clientVersion";
+        String value = "3.5.2";
+        Map<String, String> postMaps = new HashMap<>();
+        postMaps.put(key, value);
+        VolleyManager.getInstance().startRequest(postUrl, postMaps, new OnNetResultListener() {
+            @Override
+            public void onSuccessful(String resultStr) {
+                Gson gson = new Gson();
+                ChoiceEntity choiceEntity = gson.fromJson(resultStr, ChoiceEntity.class);
+//                up主推荐
+                upBeanList = choiceEntity.getData().getRecommendUp();
+//                轮播图
+                bannerBeanList = choiceEntity.getData().getBannerTop();
+//                奥斯卡
+                resultListBeanList = choiceEntity.getData().getOfficalAlbum();
+//                今日推荐
+                todayRecommendBeanList = choiceEntity.getData().getTodayRecommend();
+//                分类
+                categoryListBeanList = choiceEntity.getData().getCategoryList();
+                adapter.setBannerBeanList(bannerBeanList);
+                adapter.setCategoryListBeanList(categoryListBeanList);
+                adapter.setResultListBeanList(resultListBeanList);
+                adapter.setTodayRecommendBeanList(todayRecommendBeanList);
+                adapter.setUpBeanList(upBeanList);
+
+            }
+
+            @Override
+            public void onFailure(String errMsg) {
+                ToastTool.showDebugToast(getContext(), "解析失败");
+            }
+        });
     }
 }
