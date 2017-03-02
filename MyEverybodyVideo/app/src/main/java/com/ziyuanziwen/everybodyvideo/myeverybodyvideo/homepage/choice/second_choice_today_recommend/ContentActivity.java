dd69965eb6859fd787fd76408a7e459a073c925a
dllo <1434100874@qq.com>
2017-03-02 21:36:37 +0800

    播放视频,

--- MyEverybodyVideo/app/src/main/java/com/ziyuanziwen/everybodyvideo/myeverybodyvideo/homepage/choice/second_choice_today_recommend/ContentActivity.java
+++ MyEverybodyVideo/app/src/main/java/com/ziyuanziwen/everybodyvideo/myeverybodyvideo/homepage/choice/second_choice_today_recommend/ContentActivity.java
@@ -1,12 +1,15 @@
 package com.ziyuanziwen.everybodyvideo.myeverybodyvideo.homepage.choice.second_choice_today_recommend;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import com.android.volley.Request;
 import com.google.gson.Gson;
@@ -20,15 +23,22 @@ import com.ziyuanziwen.everybodyvideo.myeverybodyvideo.util.ToastTool;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.vov.vitamio.MediaPlayer;
+import io.vov.vitamio.widget.VideoView;
+
 /**
  * Created by dllo on 17/3/1.
  */
 
-public class ContentActivity extends BaseActivity {
+public class ContentActivity extends BaseActivity implements MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnInfoListener {
 
-    private VideoView videoView;
-    private ImageView playIv;
-    private ImageView fullScreenIv;
+    //视频地址
+    private String path = "http://baobab.wdjcdn.com/145076769089714.mp4";
+    private Uri uri;
+    private ProgressBar pb;
+    private TextView downloadRateView, loadRateView;
+    private CustomMediaController mCustomMediaController;
+    private VideoView mVideoView;
     private ImageView commentsIv;
     private ImageView likeIv;
     private ImageView cacheIv;
@@ -44,15 +54,19 @@ public class ContentActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        videoView = byView(R.id.fragment_choiceTodayRecommendVideoView);
-        playIv = byView(R.id.fragment_choiceTodayRecommendPlayIv);
-        fullScreenIv = byView(R.id.fragment_choiceTodayRecommendFullScreenIv);
+
         commentsIv = byView(R.id.fragment_choiceSecondTodayRecommendCommentsIv);
         likeIv = byView(R.id.fragment_choiceSecondTodayRecommendLikeIv);
         cacheIv = byView(R.id.fragment_choiceSecondTodayRecommendCacheIv);
         shareIv = byView(R.id.fragment_choiceSecondTodayRecommendShareIv);
         commentsTv = byView(R.id.fragment_choiceSecondTodayRecommendCommentsTv);
         recyclerView = byView(R.id.fragment_choiceSecondTodayRecommendRv);
+        mVideoView = (VideoView) findViewById(R.id.buffer);
+        mCustomMediaController = new CustomMediaController(this, mVideoView, this);
+        mCustomMediaController.setVideoName("白火锅 x 红火锅");
+        pb = (ProgressBar) findViewById(R.id.probar);
+        downloadRateView = (TextView) findViewById(R.id.download_rate);
+        loadRateView = (TextView) findViewById(R.id.load_rate);
     }
 
     @Override
@@ -66,6 +80,21 @@ public class ContentActivity extends BaseActivity {
         getPostRequest();
         recyclerView.setAdapter(adapter);
 
+        uri = Uri.parse(path);
+        mVideoView.setVideoURI(uri);//设置视频播放地址
+
+        mCustomMediaController.show(5000);
+        mVideoView.setMediaController(mCustomMediaController);
+        mVideoView.setVideoQuality(MediaPlayer.VIDEOQUALITY_HIGH);//高画质
+        mVideoView.requestFocus();
+        mVideoView.setOnInfoListener(this);
+        mVideoView.setOnBufferingUpdateListener(this);
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setPlaybackSpeed(1.0f);
+            }
+        });
 
     }
 
@@ -98,4 +127,49 @@ public class ContentActivity extends BaseActivity {
             }
         });
     }
+
+
+    @Override
+    public boolean onInfo(MediaPlayer mp, int what, int extra) {
+        switch (what) {
+            case MediaPlayer.MEDIA_INFO_BUFFERING_START:
+                if (mVideoView.isPlaying()) {
+                    mVideoView.pause();
+                    pb.setVisibility(View.VISIBLE);
+                    downloadRateView.setText("");
+                    loadRateView.setText("");
+                    downloadRateView.setVisibility(View.VISIBLE);
+                    loadRateView.setVisibility(View.VISIBLE);
+
+                }
+                break;
+            case MediaPlayer.MEDIA_INFO_BUFFERING_END:
+                mVideoView.start();
+                pb.setVisibility(View.GONE);
+                downloadRateView.setVisibility(View.GONE);
+                loadRateView.setVisibility(View.GONE);
+                break;
+            case MediaPlayer.MEDIA_INFO_DOWNLOAD_RATE_CHANGED:
+                downloadRateView.setText("" + extra + "kb/s" + "  ");
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        loadRateView.setText(percent + "%");
+
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        //屏幕切换时，设置全屏
+        if (mVideoView != null){
+            mVideoView.setVideoLayout(VideoView.VIDEO_LAYOUT_SCALE, 0);
+        }
+        super.onConfigurationChanged(newConfig);
+    }
+
 }
